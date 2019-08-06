1f1ec0cf24c29076f5c22dae804b4bd1ab26d1d2
Victor Koehne <vik_koehne@hotmail.com>
2019-08-06 13:12:36 -0300

    a

--- bin/sunimagedif.sh
+++ bin/sunimagedif.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "------------------------------------------------"
-echo "sufarithsub.sh - Gets diff of 2 su files and ximage it to user."
+echo "sunimagedif.sh - Gets diff of 2 su files and nimage it to user."
 
 if [ "$#" -ne 2 ]; then
-	echo "sufarithsub.sh: wrong number of input parameters. Exiting."
+	echo "sunimagedif.sh: wrong number of input parameters. Exiting."
 	echo "Correct usage is:"
-	echo "	sufarithsub.sh sufile1.su sufile2.su"
+	echo "	sunimagedif.sh sufile1.su sufile2.su"
 	echo "Example:"
 	echo "	sufarithsub.sh shots1.su shots2.su"
 	echo "------------------------------------------------"
@@ -16,14 +16,15 @@ fi
 echo "------------------------------------------------"
 
 n1=$(surange <$1 | awk '$1 ~ /ns/ {print $2}')
+n2=$(surange <$1 | awk '$1 ~ /trwf/ {print $2}')
 
 sustrip <$1 >f1
 sustrip <$2 >f2
 
 farith in=f1 in2=f2 out=f3 op=sub
 
-ximage n1=$n1 <f1 perc=98 legend=1 lwidth=8 title=$1 & 
-ximage n1=$n1 <f2 perc=98 legend=1 lwidth=8 title=$2 & 
-ximage n1=$n1 <f3 perc=98 legend=1 lwidth=8 title='dif' & 
+nimage n1=$n1 n2=$n2 <f1 perc=98 legend=1 lwidth=8 title=$1 & 
+nimage n1=$n1 n2=$n2 <f2 perc=98 legend=1 lwidth=8 title=$2 & 
+nimage n1=$n1 n2=$n2 <f3 perc=98 legend=1 lwidth=8 title='dif' & 
 
 rm f1 f2 f3
